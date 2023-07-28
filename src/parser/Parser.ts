d4f640ee26f4cf4aa8195caaf8df06f68546f75a
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-07-29 00:17:52 +0200

    Parser: method dispatch

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -4,18 +4,30 @@ import { ParsableString } from "./ParsableString"
 
 export type R = RegExpExecArray | null
 export type Rr = RegExpExecArray
-export type Regs = { [k: string]: RegExp }
+export type Regs = Obj<RegExp>
 export type ParsingResult = {
   level: number
   text: string
-  type: string | number
+  type: string | number | symbol
 }
 export type NextMethod = () => ParsingResult | NextMethod
 
+type FunctionDispatcher = Obj<(p: ParsingResult) => void>
+
 class Parser {
   state: ParserState
+  fDispatch: FunctionDispatcher
   constructor() {
     this.state = new ParserState()
+    this.fDispatch = {
+      heading: this.#heading,
+      list: this.#list,
+      nList: this.#nlist,
+      bSrc: this.#bsrc,
+      eSrc: this.#esrc,
+      nSrc: this.#nsrc,
+      paragraph: this.#paragraph,
+    }
   }
 
   parseOrg(buff: Buffer) {
@@ -25,9 +37,19 @@ class Parser {
 
   #qualifyLine(s: string) {
     const p = new ParsableString(s)
-    const { level, text, type } = p.start() as ParsingResult
-    console.log(text)
+    const parsed = p.start() as ParsingResult
+    this.fDispatch[parsed.type].call(this, parsed)
   }
+
+  #heading(p: ParsingResult) {
+    console.log(p.text)
+  }
+  #list(p: ParsingResult) {}
+  #nlist(p: ParsingResult) {}
+  #bsrc(p: ParsingResult) {}
+  #nsrc(p: ParsingResult) {}
+  #esrc(p: ParsingResult) {}
+  #paragraph(p: ParsingResult) {}
 }
 
 export default Parser
